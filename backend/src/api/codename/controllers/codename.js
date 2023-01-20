f117783c75c45e0a96252cd213f4a2123bbc01bf
suzannahurst <hurstsuzanna@gmail.com>
2023-01-20 18:11:50 +0000

    added codename collection

---
+++ backend/src/api/codename/controllers/codename.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * codename controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::codename.codename');
---
+++ backend/src/api/codename/routes/codename.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * codename router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::codename.codename');
